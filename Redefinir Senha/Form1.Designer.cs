7d1807614d0ceb2fc8f4ff18a61501372be66d22
Michael Batista <michaelbatista96@outlook.com>
2024-10-11 19:02:05 -0300

    redefinirSenha

---
+++ Redefinir Senha/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace Redefinir_Senha
+{
+    partial class Redefinir_Senha
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Redefinir_Senha));
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges8 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges9 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges10 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            fontDialog1 = new FontDialog();
+            textBox1 = new TextBox();
+            h1_textbox = new TextBox();
+            label1 = new Label();
+            Whatsapp = new Label();
+            GerarCodigo = new Button();
+            backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            textBox4 = new TextBox();
+            backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            guna2TextBox1 = new Guna.UI2.WinForms.Guna2TextBox();
+            guna2AnimateWindow1 = new Guna.UI2.WinForms.Guna2AnimateWindow(components);
+            guna2TextBox2 = new Guna.UI2.WinForms.Guna2TextBox();
+            guna2TextBox3 = new Guna.UI2.WinForms.Guna2TextBox();
+            guna2TextBox4 = new Guna.UI2.WinForms.Guna2TextBox();
+            guna2TextBox5 = new Guna.UI2.WinForms.Guna2TextBox();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(textBox1, "textBox1");
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.ForeColor = Color.Green;
+            textBox1.Name = "textBox1";
+            // 
+            // h1_textbox
+            // 
+            resources.ApplyResources(h1_textbox, "h1_textbox");
+            h1_textbox.BorderStyle = BorderStyle.None;
+            h1_textbox.ForeColor = Color.FromArgb(0, 192, 0);
+            h1_textbox.Name = "h1_textbox";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(label1, "label1");
+            label1.Name = "label1";
+            // 
+            // Whatsapp
+            // 
+            resources.ApplyResources(Whatsapp, "Whatsapp");
+            Whatsapp.FlatStyle = FlatStyle.System;
+            Whatsapp.Name = "Whatsapp";
+            // 
+            // GerarCodigo
+            // 
+            resources.ApplyResources(GerarCodigo, "GerarCodigo");
+            GerarCodigo.BackColor = Color.FromArgb(0, 192, 0);
+            GerarCodigo.Cursor = Cursors.Hand;
+            GerarCodigo.ForeColor = Color.White;
+            GerarCodigo.Name = "GerarCodigo";
+            GerarCodigo.UseVisualStyleBackColor = false;
+            // 
+            // textBox4
+            // 
+            resources.ApplyResources(textBox4, "textBox4");
+            textBox4.BorderStyle = BorderStyle.None;
+            textBox4.Name = "textBox4";
+            // 
+            // guna2TextBox1
+            // 
+            resources.ApplyResources(guna2TextBox1, "guna2TextBox1");
+            guna2TextBox1.BackColor = Color.FromArgb(224, 224, 224);
+            guna2TextBox1.BorderColor = Color.White;
+            guna2TextBox1.CustomizableEdges = customizableEdges1;
+            guna2TextBox1.DefaultText = "Você irá receber um código no número \r\ninformado para prosseguir para próxima etapa.\r\nClique no botão abaixo para prosseguir.";
+            guna2TextBox1.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            guna2TextBox1.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            guna2TextBox1.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            guna2TextBox1.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            guna2TextBox1.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            guna2TextBox1.ForeColor = Color.Black;
+            guna2TextBox1.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            guna2TextBox1.Multiline = true;
+            guna2TextBox1.Name = "guna2TextBox1";
+            guna2TextBox1.PasswordChar = '\0';
+            guna2TextBox1.PlaceholderText = "";
+            guna2TextBox1.SelectedText = "";
+            guna2TextBox1.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            // 
+            // guna2TextBox2
+            // 
+            resources.ApplyResources(guna2TextBox2, "guna2TextBox2");
+            guna2TextBox2.CustomizableEdges = customizableEdges3;
+            guna2TextBox2.DefaultText = "";
+            guna2TextBox2.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            guna2TextBox2.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            guna2TextBox2.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            guna2TextBox2.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            guna2TextBox2.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            guna2TextBox2.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            guna2TextBox2.Name = "guna2TextBox2";
+            guna2TextBox2.PasswordChar = '\0';
+            guna2TextBox2.PlaceholderText = "ex: contato@urbangreen.com";
+            guna2TextBox2.SelectedText = "";
+            guna2TextBox2.ShadowDecoration.CustomizableEdges = customizableEdges4;
+            // 
+            // guna2TextBox3
+            // 
+            resources.ApplyResources(guna2TextBox3, "guna2TextBox3");
+            guna2TextBox3.CustomizableEdges = customizableEdges5;
+            guna2TextBox3.DefaultText = "";
+            guna2TextBox3.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            guna2TextBox3.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            guna2TextBox3.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            guna2TextBox3.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            guna2TextBox3.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            guna2TextBox3.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            guna2TextBox3.Name = "guna2TextBox3";
+            guna2TextBox3.PasswordChar = '\0';
+            guna2TextBox3.PlaceholderText = "";
+            guna2TextBox3.SelectedText = "";
+            guna2TextBox3.ShadowDecoration.CustomizableEdges = customizableEdges6;
+            // 
+            // guna2TextBox4
+            // 
+            resources.ApplyResources(guna2TextBox4, "guna2TextBox4");
+            guna2TextBox4.CustomizableEdges = customizableEdges7;
+            guna2TextBox4.DefaultText = "";
+            guna2TextBox4.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            guna2TextBox4.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            guna2TextBox4.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            guna2TextBox4.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            guna2TextBox4.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            guna2TextBox4.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            guna2TextBox4.Name = "guna2TextBox4";
+            guna2TextBox4.PasswordChar = '\0';
+            guna2TextBox4.PlaceholderText = "";
+            guna2TextBox4.SelectedText = "";
+            guna2TextBox4.ShadowDecoration.CustomizableEdges = customizableEdges8;
+            // 
+            // guna2TextBox5
+            // 
+            resources.ApplyResources(guna2TextBox5, "guna2TextBox5");
+            guna2TextBox5.CustomizableEdges = customizableEdges9;
+            guna2TextBox5.DefaultText = "";
+            guna2TextBox5.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            guna2TextBox5.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            guna2TextBox5.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            guna2TextBox5.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            guna2TextBox5.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            guna2TextBox5.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            guna2TextBox5.Name = "guna2TextBox5";
+            guna2TextBox5.PasswordChar = '\0';
+            guna2TextBox5.PlaceholderText = "";
+            guna2TextBox5.SelectedText = "";
+            guna2TextBox5.ShadowDecoration.CustomizableEdges = customizableEdges10;
+            // 
+            // Redefinir_Senha
+            // 
+            resources.ApplyResources(this, "$this");
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            Controls.Add(guna2TextBox5);
+            Controls.Add(guna2TextBox4);
+            Controls.Add(guna2TextBox3);
+            Controls.Add(guna2TextBox2);
+            Controls.Add(guna2TextBox1);
+            Controls.Add(textBox4);
+            Controls.Add(GerarCodigo);
+            Controls.Add(Whatsapp);
+            Controls.Add(label1);
+            Controls.Add(h1_textbox);
+            Controls.Add(textBox1);
+            Name = "Redefinir_Senha";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private FontDialog fontDialog1;
+        private TextBox textBox1;
+        private TextBox h1_textbox;
+        private Label label1;
+        private Label Whatsapp;
+        private Button GerarCodigo;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private TextBox textBox4;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+        private Guna.UI2.WinForms.Guna2TextBox guna2TextBox1;
+        private Guna.UI2.WinForms.Guna2AnimateWindow guna2AnimateWindow1;
+        private Guna.UI2.WinForms.Guna2TextBox guna2TextBox2;
+        private Guna.UI2.WinForms.Guna2TextBox guna2TextBox3;
+        private Guna.UI2.WinForms.Guna2TextBox guna2TextBox4;
+        private Guna.UI2.WinForms.Guna2TextBox guna2TextBox5;
+    }
+}
---
+++ Redefinir Senha/Form1.cs
@@ -0,0 +1,25 @@
+namespace Redefinir_Senha
+{
+    public partial class Redefinir_Senha : Form
+    {
+        public Redefinir_Senha()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void p_texto_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void input_email_Layout(object sender, LayoutEventArgs e)
+        {
+
+        }
+    }
+}
